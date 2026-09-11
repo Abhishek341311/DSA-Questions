4f3367fd3b17b35a6069aae54de55b1b66b20edd
ABHISHEK RAJ <abhishek341311@gmail.com>
2026-09-11 20:33:43 +0530

    Time: 9 ms (26.4%), Space: 46.8 MB (16.93%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
@@ -3,23 +3,18 @@ class Solution {
 
         int[] freq = new int[10];
 
-        // Count frequency of each digit
         for (int digit : digits) {
             freq[digit]++;
         }
 
         int count = 0;
 
-        // Hundreds place
         for (int i = 1; i <= 9; i++) {
 
-            // Tens place
             for (int j = 0; j <= 9; j++) {
 
-                // Units place (must be even)
                 for (int k = 0; k <= 8; k += 2) {
 
-                    // Check if we have enough copies
                     int[] used = new int[10];
 
                     used[i]++;
